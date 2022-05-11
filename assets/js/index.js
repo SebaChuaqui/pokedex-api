f78feda5ffe0070cf1917bb8c99576b3b21763d0
SebaChuaqui <schuaquicano@gmail.com>
2022-05-11 15:53:45 -0300

    poke I

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    $("form").submit(function (evento) {
+
+        evento.preventDefault()
+
+        let valueInput = $("#pokemonInput").val()
+
+        $.ajax({
+            url: "https://pokeapi.co/api/v2/pokemon/" + valueInput,
+            success: function (data) {
+                let nombre = data.name
+                let imagen = data.sprites.front_default
+                let peso = data.weight
+
+                $("#pokeInfo").html(`
+                <div class="text-center">
+                <h3>${nombre}</h3>
+                <img src="${imagen}">
+                <h6>Peso: ${peso}</h6>
+            </div>
+                `)
+            }
+        })
+
+    })
+
+})
